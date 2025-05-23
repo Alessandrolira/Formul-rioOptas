7bba2995e1bca085a2ae692e9c501d8939f5cb4c
Alessandro Lira <alessandrolira09@gmail.com>
2025-05-23 09:45:30 -0300

    analytics habilitado

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${poppins.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
